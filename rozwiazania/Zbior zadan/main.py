a67a2418ff167307ee78f3a05af1617a6b256eed
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2023-11-09 10:16:46 +0100

    Init zbioru zadan

---
+++ rozwiazania/Zbior zadan/main.py
@@ -0,0 +1,57 @@
+'''
+No dobra, no to w tym pliku jest rozwiazanie wszystkich (mam nadzieje) zadan z tego pdf
+
+na samym koncu jest funkcja main, w ktorej mozna wywolac kazda funkcje ktora jest rozwiazaniem zadania
+oczywiscie najpierw wypada ja tam wywolac ale mam nadzieje ze bedziesz na tyle inteligentny by to zrobic
+'''
+
+#Rozdzial 1
+
+
+'''
+Zadanie 1.1
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# funkcja main
+if __name__ == '__main__':
+    #tutaj wywoluj swoje funkcje i inne takie
+    print('Hello world!')
